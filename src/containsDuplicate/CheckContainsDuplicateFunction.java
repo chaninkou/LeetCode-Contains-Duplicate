f11de2112f895bd88612e6816c8eb9a464d1b070
chaninkou <michaelkoulove@yahoo.com>
2019-10-27 22:00:06 -0700

    Great reivew. One method to sort with n(logn) or hashset checking.

--- src/containsDuplicate/CheckContainsDuplicateFunction.java
+++ src/containsDuplicate/CheckContainsDuplicateFunction.java
@@ -1,37 +1,42 @@
 package containsDuplicate;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CheckContainsDuplicateFunction {
 	// O(n) solution
-	public boolean containsDuplicate(int[] nums) {
-		// Use a set to store all the distinct elements
-		Set<Integer> set = new HashSet<>();
+//	public boolean containsDuplicate(int[] nums) {
+//		// Use a set to store all the distinct elements
+//		Set<Integer> set = new HashSet<>();
+//		
+//		for(int current : nums){
+//			// If it contains it, return true right away
+//			if(set.contains(current)){
+//				return true;
+//			}
+//			
+//			// If it doesn't contain it, add it to hashset
+//			set.add(current);
+//		}
+//		
+//		// Return false if it does not contain duplicates
+//		return false;
+//	}
+
+	// Nlogn solution since it needs to be sorted to work
+	public boolean containsDuplicate(int[] nums){
+		// Sort the array
+		Arrays.sort(nums);
 		
-		for(int current : nums){
-			// If it contains it, return true right away
-			if(set.contains(current)){
+		// Compare current with previous, that way we know if there is duplicates
+		for(int i = 1; i < nums.length; i++){
+			if(nums[i] == nums[i - 1]){
 				return true;
 			}
-			
-			// If it doesn't contain it, add it to hashset
-			set.add(current);
 		}
 		
-		// Return false if it does not contain duplicates
+		// return false if we dont find duplicates
 		return false;
 	}
-
-	// Nlogn solution
-	// public boolean containsDuplicate(int[] nums) {
-	// Arrays.sort(nums);
-
-	// for(int ind = 1; ind < nums.length; ind++){
-	// if(nums[ind] == nums[ind - 1]){
-	// return true;
-	// }
-	// }
-	// return false;
-	// }
 }
